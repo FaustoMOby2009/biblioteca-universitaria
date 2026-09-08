b5b3503ba601a5f4c7ca7c18c7776ce9951606d4
FaustoMOby2009 <faustovmo2009@gmail.com>
2026-09-08 16:44:42 -0600

    fix: corrige la carga de la tabla del catalogo

--- src/main/java/edu/mejiasoft/biblioteca/controller/DashboardController.java
+++ src/main/java/edu/mejiasoft/biblioteca/controller/DashboardController.java
@@ -2,22 +2,59 @@ package main.java.edu.mejiasoft.biblioteca.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import main.java.edu.mejiasoft.biblioteca.model.Bibliotecario;
+import main.java.edu.mejiasoft.biblioteca.model.Libro;
 import main.java.edu.mejiasoft.biblioteca.util.sceneManager.SceneManager;
 
 public class DashboardController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
+        configurarColumnas();
     }    
     
     private Bibliotecario bibliotecario;
     private SceneManager stage;
     
+    @FXML 
+    private TableView<Libro> tvCatalogoLibros;
+    @FXML 
+    private TableColumn<Libro, String> tvColumnIsbn;
+    @FXML 
+    private TableColumn<Libro, String> tvColumnTitulo;
+    @FXML 
+    private TableColumn<Libro, String> tvColumnAutorPrincipal;
+    @FXML 
+    private TableColumn<Libro, String> tvColumnEditorial;
+    @FXML 
+    private TableColumn<Libro, String> tvColumnYearPublicacion;
+    @FXML 
+    private TableColumn<Libro, Integer> tvColumnCopias; 
+    @FXML 
+    private TableColumn<Libro, String> tvColumnUserBibliotecario;
+    
     public DashboardController(Bibliotecario bibliotecario, SceneManager stage) {
     this.bibliotecario = bibliotecario;
     this.stage = stage;
 }
+    
+    public DashboardController() {
+    }
+    
+    private void configurarColumnas() {
+        tvColumnIsbn.setCellValueFactory(new PropertyValueFactory<>("isbn"));
+        tvColumnTitulo.setCellValueFactory(new PropertyValueFactory<>("titulo"));
+        tvColumnAutorPrincipal.setCellValueFactory(new PropertyValueFactory<>("autorPrincipal"));
+        tvColumnEditorial.setCellValueFactory(new PropertyValueFactory<>("editorial"));
+        tvColumnYearPublicacion.setCellValueFactory(new PropertyValueFactory<>("yearPublicacion"));
+        tvColumnCopias.setCellValueFactory(new PropertyValueFactory<>("copiasDisponibles"));
+        tvColumnUserBibliotecario.setCellValueFactory(new PropertyValueFactory<>("usernameBibliotecario"));
+        tvCatalogoLibros.setItems(Libro.obtenerListaLibros());
+    }
 }
